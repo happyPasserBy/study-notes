5430f76b2230cd82ebf747fdf0ce25edc26e96f9
happy passerby <13611224574@163.com>
2020-04-21 16:34:50 +0800

    调整源码笔记目录

--- src/Java-based/Jdk_source_learn/util/locks/Lock.java
+++ src/Java-based/Jdk_source_learn/util/locks/Lock.java
@@ -3,7 +3,7 @@ public interface Lock {
     void lock();
     // 尝试加锁但出现一只休眠的状况，可被中断
     void lockInterruptibly() throws InterruptedException;
-    // 尝试加锁并返回加锁是否成功
+    // 尝试加锁并返回加锁是否成功(尝试非公平加锁)
     boolean tryLock();
     // 在指定时间内尝试加锁
     boolean tryLock(long time, TimeUnit unit) throws InterruptedException;
